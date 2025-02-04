2a95fbe24fea9264940626261a908e630c1ff65b
Philippe Mortelette <philippe@bde-solutions.com>
2025-02-04 10:27:32 -0500

    feat: :sparkles: init app

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="bg-gray-500">
+      <div className="flex justify-center items-center h-screen">
+        <h1 className="text-4xl font-bold">Hello World!</h1>
+      </div>
+    </div>
+  );
+};
+
+export default App;
